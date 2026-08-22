d2de6315355cdde54346ebd0fe30d4afac7d29c9
The Era of Marketing <rishirajrateria@gmail.com>
2026-08-22 14:48:41 +0530

    feat: wire ChatbotRulesController into the chatbot module, ADMIN-guarded

--- src/modules/chatbot/chatbot.module.ts
+++ src/modules/chatbot/chatbot.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ChatbotController } from './chatbot.controller';
 import { ChatbotService } from './chatbot.service';
+import { ChatbotRulesController } from './chatbot-rules.controller';
+import { ChatbotRulesService } from './chatbot-rules.service';
 import { DatabaseModule } from '../../database/database.module';
 
 @Module({
   imports: [ConfigModule, DatabaseModule],
-  controllers: [ChatbotController],
-  providers: [ChatbotService],
+  controllers: [ChatbotController, ChatbotRulesController],
+  providers: [ChatbotService, ChatbotRulesService],
   exports: [ChatbotService],
 })
 export class ChatbotModule {}
